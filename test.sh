bd15877d5fbca9313a6b739335e4c819af852af0
BirdeeHub <robindbor@gmail.com>
2024-08-16 16:31:12 -0700

    Ok
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-OUTPATH=${1:-"$(dirname "$(realpath "$0")")/tmp"}
-# nix run --refresh --show-trace --no-write-lock-file github:BirdeeHub/nixCats-site-gen -- "$OUTPATH"
-nix run --refresh --show-trace --no-write-lock-file . -- "$OUTPATH"
+[ "$1" == "--remote" ] && shift 1 && remoteUp="true"
+SRCPATH="$(dirname "$(realpath "$0")")"
+OUTPATH=${1:-"$SRCPATH/tmp"}
+if [ "$remoteUp" == "true" ]; then
+    nix run --refresh --show-trace --no-write-lock-file github:BirdeeHub/nixCats-doc -- "$OUTPATH"
+else
+    nix run --refresh --show-trace --no-write-lock-file "$SRCPATH" -- "$OUTPATH"
+fi
