be2e504579cb344724c4d5700c55cb241fa27a3e
BirdeeHub <robindbor@gmail.com>
2024-08-16 22:02:23 -0700

    what is estonia up to now ...
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,11 @@
 {
+  description = ''
+    Useage:
+    nix run --refresh --no-write-lock-file --show-trace . -- "$SOMEOUTPATH"
+
+    generates an html + css site from the nixCats documentation
+    to either the current directory or some other given path.
+  '';
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixCats.url = "github:BirdeeHub/nixCats-nvim?dir=nix";
@@ -11,48 +18,46 @@
     forSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
   in {
     packages = forSys (system: let
-      pkgs = import nixpkgs { inherit system; };
-      docsite = let
-        genvim = let
-          categoryDefinitions = { pkgs, settings, categories, name, ... }: {
-            startupPlugins = {
-              general = with pkgs.vimPlugins; [
-                onedark-nvim
-                (nvim-treesitter.withPlugins ( plugins: with plugins; [
-                  vimdoc
-                  vim
-                  luadoc
-                  todotxt
-                  nix
-                  lua
-                  bash
-                ]))
-              ];
+      genvim = let
+        categoryDefinitions = { pkgs, settings, categories, name, ... }: {
+          startupPlugins = {
+            general = with pkgs.vimPlugins; [
+              onedark-nvim
+              (nvim-treesitter.withPlugins (plugins: with plugins; [
+                vimdoc
+                vim
+                luadoc
+                todotxt
+                nix
+                lua
+                bash
+              ]))
+            ];
+          };
+        };
+        packageDefinitions = {
+          genvim = {pkgs , ... }: {
+            settings = {};
+            categories = {
+              general = true;
             };
           };
-          packageDefinitions = {
-            genvim = {pkgs , ... }: {
-              settings = {};
-              categories = {
-                general = true;
-              };
-            };
-          };
-        in
-        nixCats.utils.baseBuilder ./. {
-          inherit nixpkgs system;
-        } categoryDefinitions packageDefinitions "genvim";
-
-        helpPath = if builtins.pathExists "${nixCats}/nixCatsHelp" then "${nixCats}/nixCatsHelp" else "${nixCats}/nix/nixCatsHelp";
-        readmePath = if builtins.pathExists "${nixCats}/../README.md" then "${nixCats}/../README.md" else "${nixCats}/README.md";
+        };
       in
-      pkgs.stdenv.mkDerivation {
+      nixCats.utils.baseBuilder ./. {
+        inherit nixpkgs system;
+      } categoryDefinitions packageDefinitions "genvim";
+
+      pkgs = import nixpkgs { inherit system; };
+      docsite = pkgs.stdenv.mkDerivation {
         name = "genNixCatsDocs";
-        src = helpPath;
-        buildPhase = ''
+        src = if builtins.pathExists "${nixCats}/nixCatsHelp" then "${nixCats}/nixCatsHelp" else "${nixCats}/nix/nixCatsHelp";
+        buildPhase = let
+          readmePath = if builtins.pathExists "${nixCats}/../README.md" then "${nixCats}/../README.md" else "${nixCats}/README.md";
+        in /* bash */ ''
           export HOME=$(mktemp -d)
           mkdir -p $out
-          ${genvim}/bin/genvim --headless --cmd "lua vim.g.nixCats_doc_out = [[$out]]" --cmd "lua vim.g.nixCats_doc_src = [[$src]]"
+          ${genvim}/bin/genvim --headless --cmd "lua vim.g.nixCats_doc_out = [[$out]]; vim.g.nixCats_doc_src = [[$src]]"
           ${pkgs.pandoc}/bin/pandoc --standalone --template ${./github-markdown-dark.html} "$(realpath "${readmePath}")" -o $out/index.html -V title="NIX CATEGORIES FOR NVIM"
           ${pkgs.pandoc}/bin/pandoc --standalone --template ${./github-markdown-dark.html} "$(realpath "${./TOC.md}")" -o $out/TOC.html -V title="nixCats.org TOC"
           cp ${mkdncss}/github-markdown-dark.css $out/github-markdown-dark.css
