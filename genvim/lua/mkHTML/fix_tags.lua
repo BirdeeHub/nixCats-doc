18a961778a2a73d4302658f1d7f9694a47259d8b
BirdeeHub <robindbor@gmail.com>
2024-09-03 14:58:24 -0700

    There's four sides to every story.
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   genvim/lua/mkHTML/fix_tags.lua

--- genvim/lua/mkHTML/fix_tags.lua
+++ genvim/lua/mkHTML/fix_tags.lua
@@ -1,5 +1,9 @@
 return function (helptags_path)
+    -- TODO: read and parse helptags
+    -- for the urls to each tag
     return function (html_lines)
+        -- TODO: make vimdoc tag links into links
+        -- TODO: make vimdoc headings able to be linked to
         return html_lines
     end
 end
