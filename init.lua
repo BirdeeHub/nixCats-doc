2f32732dd94f8fb2a5f7ee1a7e539bb15892cfc1
BirdeeHub <robindbor@gmail.com>
2024-09-03 10:23:06 -0700

    Errare humanum est.
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   init.lua
    	new file:   lua/mkHTML/init.lua

--- init.lua
+++ init.lua
@@ -5,64 +5,6 @@ require('onedark').setup {
 require('onedark').load()
 vim.cmd.colorscheme('onedark')
 
-local doc_out = vim.g.nixCats_doc_out
-local doc_src = vim.g.nixCats_doc_src
-local tohtml = require('tohtml').tohtml
-
-local linkLines = {
-    [[<body style="display: flex; flex-direction: column">]],
-    [[<div style="text-align: center;">]],
-    [[<style>]],
-    [[a { color: #1a73e8; text-decoration: none; }]],
-    [[a:visited { color: #1a73e8; }]],
-    [[a:hover { color: #155ab6; text-decoration: underline; }]],
-    [[a:active { color: #003d99; }]],
-    [[</style>]],
-    [[<a href="./index.html" style="margin-right: 10px;">HOME</a>]],
-    [[<a href="./TOC.html" style="margin-right: 10px;">TOC</a>]],
-    [[<a href="https://github.com/BirdeeHub/nixCats-nvim">REPO</a>]],
-    [[</div>]],
-    [[<div style="flex-direction: row">]],
-}
-
-local function gen_doc_file(filename)
-    local srcpath = doc_src .. "/" .. filename .. ".txt"
-    local buffer = vim.api.nvim_create_buf(true, false)
-    vim.api.nvim_buf_call(buffer, function()
-        vim.cmd.edit(srcpath)
-    end)
-    local win = vim.api.nvim_open_win(buffer, true, { split = "above" })
-    local htmlopts = { title = filename, number_lines = true }
-    local filelines = tohtml(win, htmlopts)
-
-    -- add navigation header
-    local body_index = nil
-    for i, line in ipairs(filelines) do
-        if line:find("<body.*>") then
-            body_index = i
-            break
-        end
-    end
-    if body_index then
-        table.remove(filelines, body_index)
-        for i = #linkLines, 1, -1 do
-            table.insert(filelines, body_index, linkLines[i])
-        end
-    end
-    local end_body_index = nil
-    for i = #filelines, 1, -1 do
-        if filelines[i]:find("</body>") then
-            end_body_index = i
-            break
-        end
-    end
-    if end_body_index then
-        table.insert(filelines, end_body_index, "</div>")
-    end
-
-    return filelines
-end
-
 local filetable = {
     "nixCats_installation",
     "nixCats_format",
@@ -76,9 +18,13 @@ local filetable = {
 
 local converted = {}
 
-for _, name in pairs(filetable) do
+local doc_out = vim.g.nixCats_doc_out
+
+local mkHTML = require('mkHTML')
+
+for _, name in ipairs(filetable) do
     local outfile = doc_out .. "/" .. name .. ".html"
-    converted[outfile] = gen_doc_file(name)
+    converted[outfile] = mkHTML.gen_doc_file(name)
 end
 
 for output_file, lines in pairs(converted) do
