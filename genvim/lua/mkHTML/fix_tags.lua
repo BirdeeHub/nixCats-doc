f4ab3b1c20d803f80003920eecb9cd95161c3d87
BirdeeHub <robindbor@gmail.com>
2024-09-03 19:42:17 -0700

    Revert this commit
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   genvim/lua/mkHTML/fix_tags.lua

--- genvim/lua/mkHTML/fix_tags.lua
+++ genvim/lua/mkHTML/fix_tags.lua
@@ -1,6 +1,43 @@
+local function read_file_by_lines(filename)
+    local lines = {}
+    local file = io.open(filename, "r")
+    if not file then
+        error("Could not open file: " .. filename)
+    end
+    
+    for line in file:lines() do
+        table.insert(lines, line)
+    end
+    
+    file:close()
+    return lines
+end
+
+local function mysplit(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
+---@class tag_entry
+---@field a string
+---@field b string
+---@field c string
+
+---@type tag_entry[]
+local tags = {}
+
 return function (helptags_path)
-    -- TODO: read and parse helptags
-    -- for the urls to each tag
+    local lines = read_file_by_lines(helptags_path)
+    for _, line in ipairs(lines) do
+        table.insert(tags,mysplit(line, "\t"))
+    end
+    print(vim.inspect(tags))
     -- NOTE: new_tag_root will be false for relative path
     -- string for actual value if provided
     -- will never be called with nil
