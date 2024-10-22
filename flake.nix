d6937af5b18a38e507c17e8ae31a1a0fef8d9405
BirdeeHub <robindbor@gmail.com>
2024-10-22 13:33:48 -0700

    LOTS of changes. period
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -12,8 +12,8 @@
     nixCats.url = "github:BirdeeHub/nixCats-nvim?dir=nix";
     # nixCats.url = "git+file:/home/birdee/Projects/nixCats-nvim?dir=nix";
     mkdncss = {
-      url = "github:sindresorhus/github-markdown-css";
       # url = "github:jez/pandoc-markdown-css-theme";
+      url = "github:sindresorhus/github-markdown-css";
       flake = false;
     };
   };
@@ -53,6 +53,45 @@
       } categoryDefinitions packageDefinitions "genvim";
 
       pkgs = import nixpkgs { inherit system; };
+
+      # NOTE: this is the pandoc command currently being used
+      pandocCMD = pkgs.writeShellScript "pandocCMD" ''
+        export PATH="${nixpkgs.lib.makeBinPath (with pkgs; [ coreutils pandoc ])}:$PATH"
+        do_copy=$1 nix_out=$2
+        pan_in=$3 pan_out=$4 pan_title=$5
+        $(exit "$do_copy") && {
+          cp ${mkdncss}/github-markdown-dark.css $nix_out/github-markdown-dark.css
+        } || true
+        pandoc --standalone \
+          --template ${./md/github-markdown-dark.html} \
+          -V title="$pan_title" \
+          -o "$pan_out" \
+          "$(realpath "$pan_in")"
+      '';
+      # don't mind this, just trying out other themes
+      # url = "github:jez/pandoc-markdown-css-theme";
+      pandocCMDtest = pkgs.writeShellScript "pandocCMD" ''
+        export PATH="${nixpkgs.lib.makeBinPath (with pkgs; [ coreutils pandoc haskellPackages.pandoc-sidenote ])}:$PATH"
+        do_copy=$1 nix_out=$2
+        pan_in=$3 pan_out=$4 pan_title=$5
+        $(exit "$do_copy") && {
+          mkdir -p $nix_out/css && \
+          cp ${mkdncss}/public/css/* $nix_out/css
+        } || true
+        pandoc --katex \
+          --from markdown+tex_math_single_backslash \
+          --filter pandoc-sidenote \
+          --to html5+smart \
+          --template ${mkdncss}/template.html5 \
+          --css="./css/theme.css" \
+          --css="./css/skylighting-solarized-theme.css" \
+          --toc \
+          --wrap=none \
+          -V pagetitle="$pan_title" \
+          --output "$pan_out" \
+          "$(realpath "$pan_in")"
+      '';
+
       isNixDir = builtins.pathExists "${nixCats}/nixCatsHelp";
       docsite = pkgs.stdenv.mkDerivation {
         name = "genNixCatsDocs";
@@ -61,21 +100,20 @@
           readmePath = if isNixDir then "${nixCats}/../README.md" else "${nixCats}/README.md";
         in /* bash */ ''
           export HOME=$(mktemp -d)
+          do_copy=0
           pandocGen() {
-            local pan_in=$1
-            local pan_out=$2
-            local pan_title=$3
-            ${pkgs.pandoc}/bin/pandoc --standalone --template ${./md/github-markdown-dark.html} "$(realpath "$pan_in")" -o "$pan_out" -V title="$pan_title"
+            ${pandocCMD} "$do_copy" "$out" "$@"
+            do_copy=1
           }
-          mkdir -p $out
+
           # use nvim headless and the config to generate html from nvim docs
           ${genvim}/bin/genvim --headless --cmd "lua vim.g.nixCats_doc_out = [[$out]]; vim.g.nixCats_doc_src = [[$src]]"
-          # get a basic github theme css from github for markdown pandoc-generated pages
-          cp ${mkdncss}/github-markdown-dark.css $out/github-markdown-dark.css
-          pandocGen ${readmePath} $out/index.html "NIX CATEGORIES FOR NVIM"
 
-          # all pages that arent from the main nixCats repo
-          pandocGen ${./md/TOC.md} $out/TOC.html "nixCats.org TOC"
+          # run pandoc on the readme
+          pandocGen "${readmePath}" "$out/index.html" "NIX CATEGORIES FOR NVIM"
+
+          # run pandoc on all pages that arent from the main nixCats repo
+          pandocGen "${./md/TOC.md}" "$out/TOC.html" "nixCats.org TOC"
         '';
       };
     in {
