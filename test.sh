0ca712bd1e7b07ebac0ce0a096043bf6bd65a3b1
BirdeeHub <birdee@localhost>
2024-12-03 06:45:52 -0800

    the magic is real
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,9 @@
 [ "$1" == "--remote" ] && shift 1 && remoteUp="true"
 SRCPATH="$(dirname "$(realpath "$0")")"
 OUTPATH=${1:-"$SRCPATH/tmp"}
-if [ "$remoteUp" == "true" ]; then
+if [ -n "$NIXCATSPATH" ]; then
+    nix run --refresh --show-trace --no-write-lock-file --override-input nixCats "$NIXCATSPATH" "$SRCPATH" -- "$OUTPATH"
+elif [ "$remoteUp" == "true" ]; then
     nix run --refresh --show-trace --no-write-lock-file github:BirdeeHub/nixCats-doc -- "$OUTPATH"
 else
     nix run --refresh --show-trace --no-write-lock-file "$SRCPATH" -- "$OUTPATH"
