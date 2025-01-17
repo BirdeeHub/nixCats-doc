2e35cc0fa440d4e4f903d077ade45d006edc182c
BirdeeHub <robindbor@gmail.com>
2025-01-17 06:30:59 -0800

    Arrrrgggg
    (auto-msg whatthecommit.com)
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   gen_docs/templates.nix

--- gen_docs/templates.nix
+++ gen_docs/templates.nix
@@ -8,15 +8,15 @@ writeTextFile {
   name = APPNAME;
   text = /*lua*/ ''
     #!${luajit.interpreter}
-    local nixCatsSrc = [[${nixCats}/templates/]]
+    local nixCatsSrc = [[${nixCats}]]
     local templatetable = ${nixCats.utils.n2l.toLua nixCats.utils.templates}
     local resmarkdown = ""
     for k, v in pairs(templatetable) do
       if type(v) == "table" then
         if v.path:sub(1, #nixCatsSrc) == nixCatsSrc then
             local filename = v.path:sub(#nixCatsSrc + 1)
-            local link = "https://github.com/BirdeeHub/nixCats-nvim/tree/main/templates/" .. filename
-            v.path = "nix flake init -t github.com/BirdeeHub/nixCats-nvim#" .. filename
+            local link = "https://github.com/BirdeeHub/nixCats-nvim/tree/main" .. filename
+            v.path = "nix flake init -t github.com/BirdeeHub/nixCats-nvim#" .. k
             resmarkdown = resmarkdown .. "# [" .. k .. "](" .. link .. ")\n\n"
             resmarkdown = resmarkdown .. "`" .. v.path .. "`\n\n"
             resmarkdown = resmarkdown .. v.description .. "\n\n"
